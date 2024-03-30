bc60557203bea208fab279134a86e3ae8dd11253
01fe22bca070 <01fe22bca070@kletech.ac.in>
2024-03-31 01:26:40 +0530

    extended calculator

--- Calculator.java
+++ Calculator.java
@@ -26,10 +26,13 @@ public void div(float a,float b)
 
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
+        ExtendedCalculator c = new ExtendedCalculator();
         c1.add(3, 6);
         c1.sub(3, 7);
         c1.mul(3, 7);
         c1.div(4,6);
+        c.square(4);
+        c.cube(4);
         
     }
 }
---
+++ ExtendedCalculator.java
@@ -0,0 +1,14 @@
+public class ExtendedCalculator {
+    public void square(float a)
+    {
+        float  c=a*a;
+        System.out.println("the sqaure of " +a+  " is" +c);
+        
+    }
+    public void cube(float a)
+    {
+        float  c=a*a*a;
+        System.out.println("the cube of " +a+  " is" +c);
+        
+    }
+}
